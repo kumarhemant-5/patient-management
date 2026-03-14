c4888096b61a2474eed2fbe6b00eb6b55ed9064c
Hemant Kumar <hementkumaraja@gmail.com>
2026-03-15 00:09:43 +0530

    PatientRepository extend the JPARepository which internally implemented by hibernate

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -4,8 +4,18 @@ import com.pm.patientservice.dao.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class PatientService {
     @Autowired
     private PatientRepository patientRepository;
+
+    public void getPatientById(UUID id){
+        if(patientRepository.findById(id).isEmpty()){
+            System.out.println("Patient with this id is not present");
+        }else{
+            System.out.println("Patient is present");
+        }
+    }
 }
